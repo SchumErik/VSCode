509f505c8b394f4cb912fe16b4593be952ba9dbf
Erik Schumacher <schum.erik@gmail.com>
2021-01-31 10:08:13 +0100

    first trial

---
+++ hello.py
@@ -0,0 +1,10 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+fuck = 3434
+print(f'this is my{fuck}')
+x = np.linspace(0,20,100)
+print(x)
+plt.plot(x,np.sin(x)**2)
+plt.show()
